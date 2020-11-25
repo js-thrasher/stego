a17cd12cb63f015e2ef108e6a22e4b4997093b3a
thrasherjatwit <60016743+thrasherjatwit@users.noreply.github.com>
2020-11-24 23:34:06 -0800

    Add files via upload
    Finish decoding and io, add png image

--- steganographer.py
+++ steganographer.py
@@ -1,36 +1,49 @@
-import time
-
-from skimage import data
-import napari
+from skimage import io
 from random import randint
-
-message = "Hello world"
-pic = data.astronaut()
 
 
 def main():
-    with napari.gui_qt():
-        viewer = napari.view_image(pic, rgb=True)
-    encode(pic, message)
-    with napari.gui_qt():
-        viewer = napari.view_image(pic, rgb=True)
-    viewer.show()
+    mode = input('Do you wish to encode (e) or decode (d) a message:')
+    path = input('Enter the file path of your photo: ')
+    pic = io.imread(path)
+    if mode == 'e':
+        message = input('Enter the message you wish to embed: ')
+        encode(pic, message)
+        io.imsave('photo.png', pic, )
+    elif mode == 'd':
+        print(decode(pic))
 
 
 def encode(image, word):
     cypher1 = randint(1, 5)
     cypher2 = randint(1, 5)
-    image[0][0] = [cypher1, cypher2, 0]
-    codeword = atob(word)
-    x=0
-    y=0
-    for i in range(0,len(codeword)):
-        image[x,y,2] = codeword[i]
-        x+= cypher1
-        y+= cypher2
+    codeword = atoi(word)
+    image[0][0] = [cypher1, cypher2, len(codeword)]
+    x = cypher1
+    y = cypher2
+    for i in range(0, len(codeword)):
+        image[x, y, 2] = codeword[i]
+        x += cypher1
+        y += cypher2
 
-def atob(s):
-    return ''.join([format(ord(i), "08b") for i in message])
+
+def decode(image):
+    cypher1 = image[0][0][0]
+    cypher2 = image[0][0][1]
+    size = image[0][0][2]
+    rtn = []
+    x = cypher1
+    y = cypher2
+    for i in range(0, size):
+        c = chr(image[x][y][2])
+        rtn.append(c)
+        x += cypher1
+        y += cypher2
+    return ''.join(rtn)
+
+
+def atoi(s):
+    return [ord(i) for i in list(s)]
 
 
 if __name__ == "__main__":
